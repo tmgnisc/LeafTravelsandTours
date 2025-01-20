b6805de6c486e6aa26fa23470bb153a1aedc8c4a
Nischal <tamangnischal2018@gmail.com>
2025-01-20 18:28:34 +0545

    improving landing page layout

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -178,6 +178,32 @@ const Home = () => {
         </Container>
       </section>
       <Partners />
+      <section className="call_us">
+        <Container>
+          <Row className="align-items-center">
+            <Col md="8">
+              <h5 className="title">CALL TO ACTION</h5>
+              <h2 className="heading">
+                READY FOR UNFORGETTABLE TRAVEL. REMEMBER US!
+              </h2>
+              <p className="text">
+                Embark on a journey of a lifetime with us, creating memories
+                that will last forever.
+              </p>
+            </Col>
+            <Col md="4" className="text-center mt-3 mt-md-0">
+              <a
+                href="tel:9802305614"
+                className="secondary_btn bounce"
+                rel="no"
+              >
+                Contact Us!
+              </a>
+            </Col>
+          </Row>
+        </Container>
+        <div className="overlay"></div>
+      </section>
       <Features />
       <section className="tours_section slick_slider">
         <Container>
